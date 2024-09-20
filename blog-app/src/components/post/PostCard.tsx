4c8cdf0b3c79d477211bd18c9a7dd70a67b468bb
popin <popin@pop-os.localdomain>
2024-09-20 21:09:07 +0530

    nexst studio initial push in gms repo

--- blog-app/src/components/post/PostCard.tsx
+++ blog-app/src/components/post/PostCard.tsx
@@ -21,11 +21,13 @@ const PostCard: React.FC<PostCardProps> = ({ post }) => {
           {/* <CardDescription>{post.description}</CardDescription> */}
         </CardHeader>
         <CardContent className="relative w-full h-80 md:h-64 lg:h-44">
-          <img
-            className="w-full h-full object-center object-cover "
-            src={post.coverImage}
-            alt={post.title}
-          />
+          <Link to={`/post/${post._id}`}>
+            <img
+              className="w-full h-full object-center object-cover "
+              src={post.coverImage}
+              alt={post.title}
+            />
+          </Link>
         </CardContent>
         <CardFooter className="px-3 py-4 flex flex-col justify-start items-start h-80 md:h-64 lg:h-44">
           <div className="text-sm text-white flex flex-wrap">
@@ -39,9 +41,11 @@ const PostCard: React.FC<PostCardProps> = ({ post }) => {
               </Link>
             ))}
           </div>
-          <p className="text-base font-semibold text-gray-100 group-hover:text-orange-600">
-            {post.title}
-          </p>
+          <Link to={`/post/${post._id}`} className="hover:text-orange-600">
+            <p className="text-base font-semibold text-gray-100 group-hover:text-orange-600">
+              {post.title}
+            </p>
+          </Link>
         </CardFooter>
       </Card>
     </article>
--- blog-app/src/pages/AllBlogs.tsx
+++ blog-app/src/pages/AllBlogs.tsx
@@ -11,20 +11,32 @@ import {
 import { Skeleton } from "@/components/ui/skeleton";
 import { expressApi } from "@/lib/axios-conf";
 import { Blog } from "@/types/index";
+import { randomInt } from "crypto";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const AllBlogs = () => {
   const [blogs, setBlogs] = useState<Blog[]>([]);
   const [loader, setLoader] = useState<boolean>(false);
+  const [page, setPage] = useState<number>(1);
+
+  const handleScroll = () => {
+    if (
+      document.body.scrollHeight - 300 <
+      window.scrollY + window.innerHeight
+    ) {
+      setLoader(true);
+    }
+  };
+
   useEffect(() => {
     setLoader(true);
     (async () => {
-      const response = await expressApi.get("/blog/all");
+      const response = await expressApi.get(`/blog/all?page=${page}&limit=10`);
       if (response?.status === 200) {
         console.log("All blog fetched successfully");
-        const res = response.data;
-        setBlogs(res.data);
+        const payload = response.data.data;
+        setBlogs((prev) => [...new Set([...prev, ...payload.data])]);
         setLoader(false);
       } else if (response?.status === 404) {
         console.log("No blog found");
@@ -34,7 +46,30 @@ const AllBlogs = () => {
         setLoader(false);
       }
     })();
-  }, []);
+  }, [page]);
+
+  const handleLoadMore = () => {
+    setPage((prev) => prev + 1);
+  };
+
+  useEffect(() => {
+    if (loader) handleLoadMore();
+  }, [loader]);
+
+  const debounce: any = (func: (...args: any) => void, delay: number) => {
+    let timeoutId: NodeJS.Timeout | null = null;
+    return function (...args: any) {
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+      }
+      timeoutId = setTimeout(() => {
+        func(...args);
+      }, delay);
+    };
+  };
+
+  window.addEventListener("scroll", debounce(handleScroll, 500));
+
   return (
     <div className="w-full m-5 flex flex-col justify-center items-center gap-2">
       <div className="col-span-5">All Blogs</div>
@@ -48,12 +83,8 @@ const AllBlogs = () => {
       <section className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-4 mt-12 mb-12">
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-8">
           {!loader &&
-            blogs.map((blog) => {
-              return (
-                <Link to={`/post/${blog._id}`} key={blog._id}>
-                  <PostCard post={blog} />
-                </Link>
-              );
+            blogs.map((blog, i) => {
+              return <PostCard post={blog} key={blog._id + i} />;
             })}
         </div>
       </section>
